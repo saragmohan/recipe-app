a9d60514393f5de8852b1f4b561f47b540244bdd
Sarag Mohan P <sarag.mohan@gmail.com>
2022-12-26 00:05:55 +0530

    React card icons added and styles updated

--- src/components/recipe cards/RecipeCard.js
+++ src/components/recipe cards/RecipeCard.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Group, Time } from "../../assets";
 
 const RecipeCard = () => {
   return (
     <div className="recipeCard">
       <img className="recipeImg" alt=""></img>
       <span className="recipeName">Title</span>
-      <div></div>
+      <div className="recipeInfo">
+        <Time />
+        <span>10 mins</span>
+        <Group />
+        <span>4</span>
+      </div>
     </div>
   );
 };
